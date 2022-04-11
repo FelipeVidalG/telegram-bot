0538e1a900fbf8df0b5fda02ba2d702588010ca9
FelipeVidalG <felipevgevaerd@gmail.com>
2022-04-11 13:17:08 -0300

    alterações chat id

--- bot_telegram.py
+++ bot_telegram.py
@@ -16,28 +16,67 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 numero = ''
 
 global opcoes_handler
-opcoes_handler = ''
+# opcoes_handler = ''
 
 # responde o command messages /
 
 # solicita contato para o user e começa robô
 def start(update: Update, context: CallbackContext):
+        global chat_id
+        chat_id = str(update.message.chat_id)
+        banco = mysql.connector.connect(host='',database='saw_teste',user='root',password='')
+        cursor = banco.cursor(buffered=True)
+        
+        busca_opcoes = cursor.execute("SELECT * FROM teste_telegram")
+        opcoes = cursor.fetchall()
+
+        busca_menu = cursor.execute("SELECT * FROM menu_telegram ORDER BY sequencia")
+        menu_resultado = cursor.fetchall()
+
         global botao
         global custom
-
-        if opcoes_handler != '':
-                dispatcher.remove_handler(opcoes_handler)
-
-
         
         botao = telegram.KeyboardButton('Mandar contato', request_contact = True)
         custom = [[botao]]
         reply_markup = telegram.ReplyKeyboardMarkup(custom)
+
+        if opcoes_handler != '':
+                dispatcher.remove_handler(opcoes_handler)
+
+        menu_for = []
+        for i in menu_resultado:
+                menu_desc =  str(i[3])
+                menu_for = str(menu_for) + f"""\n{menu_desc}"""
+
+        simbolos = ["[", "]"]
+        for c in simbolos:
+                menu_for = menu_for.replace(c,'')
+
+        
+
+        global menu
+        text = "Digite um dos números abaixo para escolher uma das opções:"
+        menu = f"""{text}
+                {menu_for}"""  
+
+        for i in opcoes:
+                if str(i[4])==chat_id:
+                        context.bot.send_message(chat_id=update.effective_chat.id, text=menu)  
+                        global mandar_opcoes_handler
+                        # mandar_opcoes_handler = MessageHandler(Filters.text, mandar_opcoes)
+                        dispatcher.add_handler(opcoes_handler)
+                        # dispatcher.remove_handler(tente_novamente_handler)
+                        return False
+        
+        
+        mandar_opcoes_handler = MessageHandler(Filters.contact, mandar_opcoes)
         context.bot.send_message(chat_id=update.effective_chat.id, text="""
         Ola! Aqui é o robô da TireShop.
         Envie seu contato para podemos prosseguir:
         """,reply_markup=reply_markup)  
-        
+        dispatcher.add_handler(mandar_opcoes_handler)
+
+
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
@@ -49,12 +88,11 @@ def mandar_opcoes(update: Update, context: CallbackContext):
         # chat_id=update.effective_chat.id -> localização da mensagem (os chats com updates)
         # update.message.text --> ultimo mensagem do chat 
         # dispatcher.remove_handler(start_handler)
-        banco = mysql.connector.connect(host='',database='saw_teste',user='root',password='')
+        banco = mysql.connector.connect(host='192.168.10.82',database='saw_teste',user='root',password='rapadura')
         cursor = banco.cursor(buffered=True)
-        busca_opcoes = cursor.execute("SELECT * FROM menu_telegram")
+        busca_opcoes = cursor.execute("SELECT * FROM menu_telegram ORDER BY sequencia")
         opcoes = cursor.fetchall()
         menu_for = []
-        text = "Digite um dos números abaixo para escolher uma das opções"
         for i in opcoes:
                 menu_id = str(i[0])
 
@@ -66,16 +104,12 @@ def mandar_opcoes(update: Update, context: CallbackContext):
         for c in simbolos:
                 menu_for = menu_for.replace(c,'')
 
-        menu = f"""Digite um dos números abaixo para escolher uma das opções:
+        
+
+        global menu
+        text = "Digite um dos números abaixo para escolher uma das opções:"
+        menu = f"""{text}
                 {menu_for}"""  
-#         menu = f"""
-#         Digite um dos números abaixo para escolher uma das opções
-#         {opcoes[0][0]} {opcoes[0][3]}
-#        {opcoes[1][0]} {opcoes[1][3]}
-#         {opcoes[2][0]}  {opcoes[2][3]}
-#         {opcoes[3][0]}  {opcoes[3][3]}
-#         """
-      
 
 
         reply_markup = telegram.ReplyKeyboardRemove(custom)
@@ -83,9 +117,12 @@ def mandar_opcoes(update: Update, context: CallbackContext):
         global numero
         global first_name
         global last_name
-        numero = update.message.contact.phone_number
-        first_name = update.message.contact.first_name
-        last_name = update.message.contact.last_name
+        try:
+                numero = update.message.contact.phone_number
+                first_name = update.message.contact.first_name
+                last_name = update.message.contact.last_name
+        except:
+                pass
         # print(update.message.contact)
         # print(f"{numero} e {first_name} {last_name}")
         salvar()
@@ -101,21 +138,23 @@ def salvar():
         # cursor.execute("CREATE TABLE users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, numero text, first_name text, last_name text)")
         busca_numeros = cursor.execute("SELECT numero FROM teste_telegram")
         numeros = cursor.fetchall()
+        dispatcher.remove_handler(mandar_opcoes_handler)
 
-        if len(numero) > 0:
-                global opcoes_handler
-                opcoes_handler = MessageHandler(Filters.text, opcoes)
-                dispatcher.add_handler(opcoes_handler)
-                dispatcher.remove_handler(tente_novamente_handler)
+        global opcoes_handler
+        opcoes_handler = MessageHandler(Filters.text, opcoes)
+        dispatcher.add_handler(opcoes_handler)
+        # dispatcher.remove_handler(tente_novamente_handler)
         for i in numeros:
                 if str(i[0])==numero:
                         return False
 
-
-        if last_name != None:
-                cursor.execute(f"INSERT INTO teste_telegram (numero,first_name,last_name) VALUES('{numero}','{first_name}','{last_name}')")
-        else:
-                cursor.execute(f"INSERT INTO teste_telegram (numero,first_name,last_name) VALUES('{numero}','{first_name}','VAZIO')")
+        try:
+                if last_name != None:
+                        cursor.execute(f"INSERT INTO teste_telegram (numero,first_name,last_name,chat_id) VALUES('{numero}','{first_name}','{last_name}','{chat_id}')")
+                else:
+                        cursor.execute(f"INSERT INTO teste_telegram (numero,first_name,last_name,chat_id) VALUES('{numero}','{first_name}','VAZIO','{chat_id}')")
+        except:
+                pass
 
 
         banco.commit()
@@ -132,14 +171,7 @@ def opcoes(update: Update, context: CallbackContext):
         elif update.message.text == '4':
                 context.bot.send_document(chat_id=update.effective_chat.id, document=open('files/report.pdf', 'rb'))
         else:
-                 context.bot.send_message(chat_id=update.effective_chat.id,text="""
-        Opção não existente, tente novamente
-        Digite um dos números abaixo para escolher uma das opções
-        1 --> você escolheu a opção 1
-        2 --> você escolheu a opção 2
-        3 --> você escolheu a opção 3
-        4 --> você escolheu a opção 4
-        """)
+                 context.bot.send_message(chat_id=update.effective_chat.id,text=f"""Opção inválida, tente novamente:\n{menu}""")
 
 def tente_novamente(update: Update, context: CallbackContext):
         context.bot.send_message(chat_id=update.effective_chat.id, text="""
@@ -148,12 +180,13 @@ def tente_novamente(update: Update, context: CallbackContext):
         context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('files/icone_contato.png', 'rb'))
         
 
-tente_novamente_handler = MessageHandler(Filters.text, tente_novamente)
-dispatcher.add_handler(tente_novamente_handler)
+# tente_novamente_handler = MessageHandler(Filters.text, tente_novamente)
+# dispatcher.add_handler(tente_novamente_handler)
 
-mandar_opcoes_handler = MessageHandler(Filters.contact, mandar_opcoes)
-dispatcher.add_handler(mandar_opcoes_handler)
+# mandar_opcoes_handler = MessageHandler(Filters.contact, mandar_opcoes)
+# dispatcher.add_handler(mandar_opcoes_handler)
 
+opcoes_handler = MessageHandler(Filters.text, opcoes)
 # opcoes_handler = MessageHandler(Filters.text, opcoes)
 # dispatcher.add_handler(opcoes_handler)
 
